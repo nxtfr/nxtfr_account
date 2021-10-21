39fc487791554063864268f338c7a0787d371095
Cflow <christian@flodihn.se>
2021-10-21 21:56:16 +0200

    Added missing event handler.

---
+++ src/nxtfr_account_event_handler.erl
@@ -0,0 +1,28 @@
+-module(nxtfr_account_event_handler).
+-author("christian@flodihn.se").
+-behaviour(gen_event).
+
+-export([init/1, handle_event/2, handle_call/2, handle_info/2, code_change/3, terminate/2]).
+ 
+init([]) ->
+    {ok, []}.
+ 
+handle_event({register, {email, Email}, {password, Password}, {extra, Extra}}, State) ->
+    io:format("AccountSystem: Received register ~p.~n", [Email]),
+    nxtfr_account:create(Email, Password, Extra)
+    {ok, State};
+
+handle_event(_, State) ->
+    {ok, State}.
+ 
+handle_call(_, State) ->
+    {ok, ok, State}.
+ 
+handle_info(_, State) ->
+    {ok, State}.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+ 
+terminate(_Reason, _State) ->
+    ok.
